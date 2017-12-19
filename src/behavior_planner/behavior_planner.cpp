c2a0a508366ed256e9382d3a7bd8664a6b947fe2
Jaya Kasa <kasa288@gmail.com>
2017-12-19 14:35:31 -0500

    working on scoring function

--- src/behavior_planner/behavior_planner.cpp
+++ src/behavior_planner/behavior_planner.cpp
@@ -4,75 +4,117 @@
 
 #include "behavior_planner.h"
 
-trajectory_option* behavior_planner::highwayPlanningAtD(short lane, double distance){
-    trajectory_option* option = new trajectory_option();
+void behavior_planner::calculateS(short lane, double &score_s, VectorXd &s){
+    double sf = 0, sf_dot = 0, sf_dot_dot = 0;
+    getSfVals(sf, sf_dot, lane);
 
-    // calculate for s and d separately for each time t --> more costly
-    double sf = 0, sf_dot = 0, sf_dot_dot = 0, df = calculateTargetD(lane), df_dot = 0, df_dot_dot = 0;
-    for(short a = 0; a <= num_points; a++){
-        // cout << "time: " << time << endl;
-        // calculate sf values
-        getSfVals(sf, sf_dot, lane);
-        // cout << "s: " << car->getS() << " s_dot: " << car->getVelocityS() << " s_dot_dot: " << car->getAccelerationS() << endl;
-        // cout << "sf: " << sf << " sf_dot: " << sf_dot << " sf_dot_dot: " << sf_dot_dot << endl;
-        // generate matrices
+    for(double time = 1.0; time <= 5.0; time+=refresh_rate) {
+        // cout << "time: " << time;
+        // cout << "CALCULATE S" << endl;// generate matrices
         VectorXd c_s = VectorXd::Zero(3);
-        // cout << "CALCULATE S" << endl;
-        sharedCalc(a, car->getS(), car->getVelocityS(), car->getAccelerationS(), sf, sf_dot, sf_dot_dot, c_s);
+        sharedCalc(time, car->getS(), car->getVelocityS(), car->getAccelerationS(), sf, sf_dot, sf_dot_dot, c_s);
         // cout << c_s.transpose() << endl;
+        // cout << "\tS -> jerk: " << c_s[0] << " snap: " << c_s[1] << " crackle: " << c_s[2] << endl;
 
         // calculate cost
-        float cost = numeric_limits<float>::max();
-        if(values->getBehavior(lane) == KEEP_VELOCITY && c_s[0] < max_jerk){
-            cost = costS(lane, a, sf, c_s);
-        } else {
-            cost = costS(lane, a, sf_dot, c_s);
-        }
-        if(cost < option->score_s){
-            option->score_s = cost;
+        // cout << "A ";
+        if (c_s[0] < max_jerk && c_s[0] > -max_jerk) {
+            // cout << "B ";
+            if(sf_dot > car->getVelocityS() && c_s[0] < 0){
+                break; // skip and jerk values that have me moving in the wrong direction
+            }
+            if(sf_dot < car->getVelocityS() && c_s[0] > 0){
+                break; // skip and jerk values that have me moving in the wrong direction
+            }
+            if(sf_dot == car->getVelocityS() && c_s[0] != 0){
+                break; // skip and jerk values that have me moving in the wrong direction
+            }
 
-            VectorXd s = VectorXd::Zero(6);
-            s << car->getS(), car->getVelocityS(), car->getAccelerationS(), c_s[0], c_s[1], c_s[2];
-            option->s = s;
-            // cout << "S: " << s.transpose() << endl;
+            float cost;
+            if (values->getBehavior(lane) == KEEP_VELOCITY) {
+                // cout << "C ";
+                cost = costS(lane, time, sf, c_s);
+            } else {
+                // cout << "D ";
+                cost = costS(lane, time, sf_dot, c_s);
+            }
+            // cout << "E ";
+            if (cost < score_s) {
+                // cout << "F " << endl;
+                score_s = cost;
+                // cout << "cost: " << cost << endl;
+                VectorXd s_temp = VectorXd::Zero(6);
+                s_temp << car->getS(), car->getVelocityS(), car->getAccelerationS(), c_s[0], c_s[1], c_s[2];
+                s = s_temp;
+                // cout << "S: " << s.transpose() << endl;
+            }
+            // cout << "G ";
+        }else {
+            if((abs(c_s[0]) - max_jerk) > 5){
+                time += refresh_rate; // speed up the calculations
+            }
         }
-        // cout << "jerk for S: " << c_s[0] << " snap: " << c_s[1] << " crackle: " << c_s[2] << " cost: " << cost << endl;
+        // cout << endl;
+    }
+}
+
+void behavior_planner::calculateD(short lane, double &score_d, VectorXd &d){
+    double df = calculateTargetD(lane), df_dot = 0, df_dot_dot = 0;
+
+    for(double time = 1.0; time <= 5.0; time+=refresh_rate){
+        // cout << "time: " << time;
 
         VectorXd c_d = VectorXd::Zero(3);
         // cout << "CALCULATE D" << endl;
-        sharedCalc(a, car->getD(), car->getVelocityD(), car->getAccelerationD(), df, df_dot, df_dot_dot, c_d);
+        sharedCalc(time, car->getD(), car->getVelocityD(), car->getAccelerationD(), df, df_dot, df_dot_dot, c_d);
         // cout << c_d.transpose() << endl;
+
+        // cout << "\tD -> jerk: " << c_d[0] << " snap: " << c_d[1] << " crackle: " << c_d[2] << endl;
 
         double diff = df - car->getD();
         // calculate cost
-        cost = numeric_limits<float>::max();
-        if(c_d[0] < max_jerk){
-            cost = costD(lane, a, diff, c_d);
-        }
-        if(cost < option->score_d){
-            option->score_d = cost;
+        if(c_d[0] < max_jerk && c_d[0] > -max_jerk){
+            if(car->getLane() < lane && c_d[0] < 0){ // turn right
+                break; // skip and jerk values that have me moving in the wrong direction
+            }
+            if(car->getLane() > lane && c_d[0] > 0){ // turn left
+                break; // skip and jerk values that have me moving in the wrong direction
+            }
+            if(car->getLane() == lane && c_d[0] != 0){ // stay in lane
+                break; // skip and jerk values that have me moving in the wrong direction
+            }
 
-            VectorXd d = VectorXd::Zero(6);
-            d << car->getD(), car->getVelocityD(), car->getAccelerationD(), c_d[0], c_d[1], c_d[2];
-            option->d = d;
-            // cout << "D: " << d.transpose() << endl;
+            float cost = costD(lane, time, diff, c_d);
+            if(cost < score_d){
+                score_d = cost;
+                // cout << "cost: " << cost << endl;
+                VectorXd d_temp = VectorXd::Zero(6);
+                d_temp  << car->getD(), car->getVelocityD(), car->getAccelerationD(), c_d[0], c_d[1], c_d[2];
+                d = d_temp;
+                // cout << "D: " << d.transpose() << endl;
+            }
         }
-
-        // cout << "jerk for D: " << c_d[0] << " snap: " << c_d[1] << " crackle: " << c_d[2] << " cost: " << cost << "\n" <<endl;
+        else {
+            if((abs(c_d[0]) - max_jerk) > 5){
+                time += refresh_rate; // speed up the calculations
+            }
+        }
     }
-
-    // cout << "highwayPlanning: lowest s score so far: " << option->score_s << "\t\tlowest d score so far: " << option->score_d << endl;
-
-    return option;
 }
 
 trajectory_option* behavior_planner::highwayPlanning(short lane){
-    // println("behavior_planner::highwayPlanning ");
-    // cout << lane << endl;
-    // values->printScores();
-    // car->print();
+    trajectory_option* option = new trajectory_option();
+    // calculate sf values
+    // cout << "s: " << car->getS() << " s_dot: " << car->getVelocityS() << " s_dot_dot: " << car->getAccelerationS() << endl;
+    // cout << "sf: " << sf << " sf_dot: " << sf_dot << " sf_dot_dot: " << sf_dot_dot << endl;
 
-    return new trajectory_option;
+    future<void> calc_s = async(launch::deferred, [this, lane, option]{ calculateS(lane, option->score_s, option->s);});
+    future<void> calc_d = async(launch::deferred, [this, lane, option]{ calculateD(lane, option->score_d, option->d);});
+
+    calc_s.get();
+    calc_d.get();
+    // cout << "highwayPlanning: lowest s score so far: " << option->score_s << "\t\tlowest d score so far: " << option->score_d << endl;
+    return option;
 }
 
 trajectory_option behavior_planner::cityPlanning(short lane){
@@ -84,7 +126,7 @@ trajectory_option behavior_planner::cityPlanning(short lane){
 }
 
 vector<VectorXd> behavior_planner::bestOption(){
-    // println("behavior_planner::bestOption");
+    cout << "behavior_planner::bestOption" << endl;
     vector<future<trajectory_option>> options;
 
     for(short a = 0; a < num_lanes; a++){
@@ -106,15 +148,14 @@ vector<VectorXd> behavior_planner::bestOption(){
     // cannot compare without the completed computations
     // this part is only as slow as the slowest calculation
 
-    short index = 0;
-    trajectory_option lowest = options[index].get();
+    trajectory_option lowest = options[0].get();
     VectorXd s  = lowest.s;
     VectorXd d  = lowest.d;
     double score_s = lowest.score_s;
     double score_d = lowest.score_d;
-
+    cout << "lowest s score so far: " << score_s << "\t\tlowest d score so far: " << score_d << endl;
     for(short a = 1; a < options.size(); a++){
-        // cout << "index: " << index << " a: " << a << endl;
+        cout << "a: " << a << endl;
         try{
             lowest = options[a].get();
             if(score_s > lowest.score_s && score_d > lowest.score_d){
@@ -132,6 +173,8 @@ vector<VectorXd> behavior_planner::bestOption(){
     }
 
     // cout << "picked the best option" << endl;
+    cout << "S: " << s.transpose() << endl;
+    cout << "D: " << d.transpose() << endl;
 
     return {s, d};
 }
--- src/behavior_planner/behavior_planner.h
+++ src/behavior_planner/behavior_planner.h
@@ -34,7 +34,6 @@ class behavior_planner {
 private:
     driver *car;
     scores *values;
-    vector<MatrixXd> times;
 
     inline void getSfVals(double &sf, double &sf_dot, short lane){
         // values->printScores();
@@ -67,53 +66,46 @@ private:
     }
 
     // cost functions
-    inline double costS(short lane, short index, double diff, VectorXd &calculated){
-        double time = time_period + refresh_rate * index;
-        cout << "behavior_planner::costS: [ k_j = " << k_j << ", jerk^2 = " << squareJerk(calculated, time) << ", kt = " << k_t << ", time = " << time << ", ks = " << k_s << ", diff^2 = " << pow(diff, 2) << " ]" << endl;
+    inline double costS(short lane, double time, double diff, VectorXd &calculated){
+        // cout << "\tbehavior_planner::costS: [ k_j = " << k_j << ", jerk^2 = " << squareJerk(calculated, time) << ", kt = " << k_t << ", time = " << time << ", ks = " << k_s << ", diff^2 = " << pow(diff, 2) << " ]" << endl;
         // cout << calculated.transpose() << endl;
         return k_j * squareJerk(calculated, time) + k_t * time + k_s * pow(diff, 2);
     }
 
-    inline double costD(short lane, short index, double diff, VectorXd &calculated){
-        double time = time_period + refresh_rate * index;
-        cout << "behavior_planner::costD: [ k_j = " << k_j << ", jerk^2 = " << squareJerk(calculated, time) << ", kt = " << k_t << ", time = " << time << ", kd = " << k_d << ", diff^2 = " << pow(diff, 2) << " ]" << endl;
+    inline double costD(short lane, double time, double diff, VectorXd &calculated){
+        // cout << "\tbehavior_planner::costD: [ k_j = " << k_j << ", jerk^2 = " << squareJerk(calculated, time) << ", kt = " << k_t << ", time = " << time << ", kd = " << k_d << ", diff^2 = " << pow(diff, 2) << " ]" << endl;
         // cout << calculated.transpose() << endl;
         return k_j * squareJerk(calculated, time) + k_t * time + k_d * pow(diff, 2);
     }
 
-    inline double costSport(short &lane, VectorXd &calculated){
-        // println("behavior_planner::costSport");
-        return  0;
-    }
-
-    inline double costEconomy(short &lane, VectorXd &calculated){
-        // println("behavior_planner::costEconomy");
-        return  0;
-    }
-
     // behavior based on velocity
+
+    void calculateS(short lane, double &cost, VectorXd &s);
+
+    void calculateD(short lane, double &score_d, VectorXd &d);
 
     trajectory_option* highwayPlanning(short lane);
 
-    trajectory_option* highwayPlanningAtD(short lane, double distance);
-
     trajectory_option cityPlanning(short lane);
 
-    inline void sharedCalc(short index, double x, double x_dot, double x_dot_dot, double xf, double xf_dot, double xf_dot_dot, VectorXd &c){
+    inline void sharedCalc(double time, double x, double x_dot, double x_dot_dot, double xf, double xf_dot, double xf_dot_dot, VectorXd &c){
         // cout << "sharedCalc: [ time = " << time << ", x = " << x << ", x_dot = " << x_dot << ", x_dot_dot = " << x_dot_dot << ", xf = " << xf << ", xf_dot = " << xf_dot << ", xf_dot_dot = " << xf_dot_dot << endl;
         // cout << "\nA^-1:\n" << Ai << endl;
 
-        VectorXd b = VectorXd::Zero(3);
+        MatrixXd A = MatrixXd::Zero(3, 3);
+        VectorXd B = VectorXd::Zero(3);
 
-        double time = time_period + refresh_rate * index;
+        A << pow(time, 3), pow(time, 4), pow(time, 5),
+                3 * pow(time, 2), 4 *pow(time, 3), 5 * pow(time, 4),
+                6 * time, 12 * pow(time, 2), 20 * pow(time, 3);
 
-        b << (xf - (x + x_dot * time + 0.5 * x_dot_dot * pow(time, 2))),
+        B << (xf - (x + x_dot * time + 0.5 * x_dot_dot * pow(time, 2))),
                 (xf_dot - (x_dot + x_dot_dot * time)),
                 (xf_dot_dot - x_dot_dot);
 
-        // cout << "b = " << b.transpose() << endl;
+        // cout << "B = " << B.transpose() << endl;
 
-        c = times.at(index) * b;
+        c = A.inverse() * B;
         // cout << c.transpose() << endl;
     }
 
@@ -125,20 +117,7 @@ public:
         this->values = &values;
     }
 
-    ~behavior_planner(){
-        for(short a = 0; a <= num_points; a++){
-            double time = time_period + refresh_rate * a;
-            MatrixXd A = MatrixXd::Zero(3, 3);
-            A << pow(time, 3), pow(time, 4), pow(time, 5),
-                    3 * pow(time, 2), 4 *pow(time, 3), 5 * pow(time, 4),
-                    6 * time, 12 * pow(time, 2), 20 * pow(time, 3);
-
-            // cout << "A" << endl;
-            // cout << A << endl;
-            times.push_back(A.inverse());
-        }
-        // cout << times.size() << endl;
-    }
+    ~behavior_planner(){}
 
     vector<VectorXd> bestOption();
 };
